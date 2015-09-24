72efef2d88bd83681ad704797e169a8fc69cff3d
kencytonn <kmutua@cytonn.com>
2015-09-24 19:06:46 +0300

    Book Controller Test

--- js/service/books_retrieve.js
+++ js/service/books_retrieve.js
@@ -1,9 +1,10 @@
-app.factory('books',['$http', function ($http){
-	return{
-		get: function(response){
-		$http.get('https://api.myjson.com/bins/56w4w').success(function(data){
-			response(data);
-		});
-		}
-	}}]);
+app.factory('books', ['$http', function ($http) {
+    return {
+        get: function (response) {
+            $http.get('https://api.myjson.com/bins/56w4w').success(function (data) {
+                response(data);
+            });
+        }
+    }
+}]);
 
---
+++ test/unit/controllerTest.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Kenwambua on 9/24/15.
+ */
+'use strict';
+
+
+describe('Bookstore controllers', function() {
+
+    describe('BookController', function(){
+        var scope, ctrl;
+
+        beforeEach(module('Bookstore'));
+
+        beforeEach(inject(function($controller) {
+            scope = {};
+            ctrl = $controller('BookController', {$scope:scope});
+        }));
+
+
+        it('should return "books" model with 4 books', function() {
+            expect(scope.phones.length).toBe(4);
+        });
+
+    });
+});
